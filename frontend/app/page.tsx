40daf057c35d4a6c216e47c1d5e629c7c6ab354d
Stener Nordnes <stener.nordnes@gmail.com>
2024-12-30 17:10:41 +0100

    Implement video metadata fetching and download functionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,101 +1,129 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+const BackendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function Home() {
+  const [link, setLink] = useState("");
+  const [videoMetadata, setVideoMetadata] = useState({
+    title: "",
+    thumbnail: "",
+    duration: "",
+    downloadLink: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("submitted link", link);
+
+    try {
+      const response = await fetch(`${BackendURL}/api/download`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url: link }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      const videoLength = Number(data.duration);
+      const hours = Math.floor(videoLength / 3600);
+      const minutes = Math.floor((videoLength % 3600) / 60);
+      const seconds = Math.floor(videoLength % 60);
+
+      setVideoMetadata({
+        title: data.title,
+        thumbnail: data.thumbnail,
+        duration: `${hours ? hours + "h " : ""}${
+          minutes ? minutes + "m " : ""
+        }${seconds}s`,
+        downloadLink: BackendURL + data.downloadLink,
+      });
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDownload = async () => {
+    if (!videoMetadata.downloadLink) {
+      return;
+    }
+
+    try {
+      const response = await fetch(videoMetadata.downloadLink);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const element = document.createElement("a");
+      element.href = url;
+      element.download = videoMetadata.title + ".mp4";
+      document.body.appendChild(element); // Required for this to work in FireFox
+      element.click();
+      document.body.removeChild(element); // Clean up
+      window.URL.revokeObjectURL(url); // Free up memory
+    } catch (error) {
+      console.error("Error downloading the video:", error);
+    }
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-4 items-center sm:items-start"
+        >
+          <Input
+            placeholder="Input a youtube link"
+            value={link}
+            onChange={(e) => {
+              const text = e.target.value;
+              setLink(text);
+              if (text.trim() === "" || videoMetadata.downloadLink) {
+                setVideoMetadata({
+                  title: "",
+                  thumbnail: "",
+                  duration: "",
+                  downloadLink: "",
+                });
+              }
+            }}
+          />
+        </form>
+        {videoMetadata.title && (
+          <div className="flex flex-col gap-4 items-center sm:items-start">
+            <h2 className="text-xl font-bold">{videoMetadata.title}</h2>
+            <img
+              src={videoMetadata.thumbnail}
+              alt="Video thumbnail"
+              className="object-cover rounded-md"
+              width={320}
+              height={180}
             />
-            Deploy now
+            <p>{videoMetadata.duration}</p>
+          </div>
+        )}
+
+        {videoMetadata.downloadLink && (
+          <a>
+            <button
+              onClick={handleDownload}
+              className="px-4 py-2 text-white rounded-md bg-gradient-to-tr from-blue-300 to-blue-950"
+            >
+              Download Video
+            </button>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        )}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
